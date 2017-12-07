94983cd89640e51118ea856181248685daa60c9b
John <32030483+Shahrukh9@users.noreply.github.com>
2017-12-07 08:44:53 +0530

    Add New Kotlin File

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int sum(int math,int c,int vb,int data,int gate)  {
+
+        System.out.println("Student Six Subject Marks");
+        int sum = math + c + vb + data + gate;
+        System.out.println("Sum"+sum);
+        return sum;
+    }
+    public static int percent() {
+        int per = (sum(3,3,3,3,33)/100);
+        System.out.println("Percent"+per);
+        return  per;
+    }
+    public static void grate() {
+        switch (percent()) {
+            case 1: System.out.println("Grade A");
+
+        }
+    }
+}
